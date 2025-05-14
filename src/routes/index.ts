57c92f78114ad8d8ec802b2b744c4e710b544a30
Julian Mora <faiber.mora@samtel.eu>
2025-05-14 16:34:16 -0500

    feat: add second GET endpoint with filters for name, username, email, phone, and website

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,24 +5,18 @@
 import { config } from '../config'
 import { AppOptions } from '../app'
 import { FastifyPluginAsync } from 'fastify'
-import { userLoadResponseSchema, UserLoadTypeResponseSchema } from '../schemas'
+import { defaultResponseMessageSchema, userLoadSchema, UserLoadSchema, userQueryStringSchema } from '../schemas'
 
 const index: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
   const ENTITY_NAME = 'Users'
-
-  console.log('JMORA[config]', config)
 
   fastify.route({
     method: 'POST',
     url: '/users/load',
     handler: async (req, reply) => {
       try {
-        console.log('JMORA[config.EXTERNAL_ENDPOINT]', `${config.EXTERNAL_ENDPOINT}/users`)
-
         // get user data from external API
-        const { data: users } = await fastify.axios.get<UserLoadTypeResponseSchema[]>(
-          `${config.EXTERNAL_ENDPOINT}/users`,
-        )
+        const { data: users } = await fastify.axios.get<UserLoadSchema[]>(`${config.EXTERNAL_ENDPOINT}/users`)
 
         // search IDs, emails and usernames in the database
         const existingUsers = await fastify.prisma.user.findMany({
@@ -61,13 +55,55 @@ const index: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<voi
 
         reply.send({ loaded: newUsers.length, message: 'Users loaded successfully.' })
       } catch (err) {
-        fastify.log.error(err)
-        reply.code(500).send({ error: 'Error loading users.' })
+        const { code, message } = fastify.customErrorHandler(err, ENTITY_NAME, '')
+        reply.code(code).send(message)
       }
     },
     schema: {
       tags: [ENTITY_NAME],
       summary: 'Loads users from an external API and saves them to the database.',
+      response: {
+        200: defaultResponseMessageSchema,
+      },
+    },
+  })
+
+  fastify.route({
+    method: 'GET',
+    url: '/users',
+    handler: async (req, reply) => {
+      try {
+        const { name, username, email, phone, website } = req.query as {
+          name?: string
+          username?: string
+          email?: string
+          phone?: string
+          website?: string
+        }
+        const where: any = {}
+        if (name) where.name = { contains: name, mode: 'insensitive' }
+        if (username) where.username = { contains: username, mode: 'insensitive' }
+        if (email) where.email = { contains: email, mode: 'insensitive' }
+        if (phone) where.phone = { contains: phone, mode: 'insensitive' }
+        if (website) where.website = { contains: website, mode: 'insensitive' }
+
+        const users = await fastify.prisma.user.findMany({ where })
+        reply.send(users)
+      } catch (err) {
+        const { code, message } = fastify.customErrorHandler(err, ENTITY_NAME, '')
+        reply.code(code).send(message)
+      }
+    },
+    schema: {
+      tags: [ENTITY_NAME],
+      summary: 'Retrieves the list of users stored in the database.',
+      querystring: userQueryStringSchema,
+      response: {
+        200: {
+          type: 'array',
+          items: userLoadSchema,
+        },
+      },
     },
   })
 }
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -5,39 +5,60 @@
 import { Static, Type } from '@sinclair/typebox'
 
 // SCHEMAS
-const userLoadResponseSchema = Type.Object({
-  id: Type.Number(),
-  name: Type.String(),
-  username: Type.String(),
-  email: Type.String(),
-  address: Type.Object({
-    street: Type.String(),
-    suite: Type.String(),
-    city: Type.String(),
-    zipcode: Type.String(),
-    geo: Type.Object({
-      lat: Type.String(),
-      lng: Type.String(),
-    }),
-  }),
-  phone: Type.String(),
-  website: Type.String(),
-  company: Type.Object({
+const userLoadSchema = Type.Object(
+  {
+    id: Type.Number(),
     name: Type.String(),
-    catchPhrase: Type.String(),
-    bs: Type.String(),
-  }),
+    username: Type.String(),
+    email: Type.String(),
+    address: Type.Object({
+      street: Type.String(),
+      suite: Type.String(),
+      city: Type.String(),
+      zipcode: Type.String(),
+      geo: Type.Object({
+        lat: Type.String(),
+        lng: Type.String(),
+      }),
+    }),
+    phone: Type.String(),
+    website: Type.String(),
+    company: Type.Object({
+      name: Type.String(),
+      catchPhrase: Type.String(),
+      bs: Type.String(),
+    }),
+  },
+  { description: 'User data from external API' },
+)
+
+const defaultResponseMessageSchema = Type.Object({
+  loaded: Type.Number({ description: 'Number of users loaded' }),
+  message: Type.String({ description: 'Load process result message' }),
 })
 
+const userQueryStringSchema = Type.Object(
+  {
+    name: Type.Optional(Type.String({ minLength: 1, description: 'Filter by name' })),
+    username: Type.Optional(Type.String({ minLength: 1, description: 'Filter by username' })),
+    email: Type.Optional(Type.String({ minLength: 1, description: 'Filter by email' })),
+    phone: Type.Optional(Type.String({ minLength: 1, description: 'Filter by teléfono' })),
+    website: Type.Optional(Type.String({ minLength: 1, description: 'Filter by website' })),
+  },
+  { description: 'Parámetros de búsqueda para usuarios' },
+)
+
 // TYPES
-type UserLoadTypeResponseSchema = Static<typeof userLoadResponseSchema>
+type UserLoadSchema = Static<typeof userLoadSchema>
 
 export {
   // SCHEMAS
-  userLoadResponseSchema,
+  userLoadSchema,
+  defaultResponseMessageSchema,
+  userQueryStringSchema,
 }
 
 export type {
   // TYPES
-  UserLoadTypeResponseSchema,
+  UserLoadSchema,
 }
